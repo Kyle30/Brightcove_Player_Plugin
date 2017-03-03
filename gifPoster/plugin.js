aeeb3982b07ef2925bf2997c5f4a5ea48fb735f2
Kyle30 <kyle30.lin@gmail.com>
2017-03-03 16:31:44 +0800

    Create plugin.js

---
+++ gifPoster/plugin.js
@@ -0,0 +1,13 @@
+videojs.plugin('gifDev',function(options){
+	var player = this;
+	player.one('loadstart',function(){
+		var vjsPoster = document.getElementsByClassName('vjs-poster');
+// 		remove - Original Poster
+		vjsPoster.style = '';
+// 		append - new Poster
+		var gifImg = document.createElement('img');
+		gifImg.style = options.style;
+		gifImg.src = options.src;
+		vjsPoster.appendChild(gifImg);
+	});
+});
