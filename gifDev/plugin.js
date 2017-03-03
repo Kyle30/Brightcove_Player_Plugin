cf498bd76644a9696089ffa81575c9e9dec34bc5
Kyle30 <kyle30.lin@gmail.com>
2017-03-03 16:08:52 +0800

    Update plugin.js

--- gifDev/plugin.js
+++ gifDev/plugin.js
@@ -1,11 +1,13 @@
 videojs.plugin('gifDev',function(options){
 	var player = this;
 	player.one('loadstart',function(){
-		var poster = $('.vjs-poster');
-		poster.style = '';
+		var vjsPoster = document.getElementsByClassName('vjs-poster');
+// 		remove - Original Poster
+		vjsPoster.style = '';
+// 		append - new Poster
 		var gifImg = document.createElement('img');
 		gifImg.style = options.style;
 		gifImg.src = options.src;
-		poster.append(gifImg);
+		vjsPoster.append(gifImg);
 	});
 });
