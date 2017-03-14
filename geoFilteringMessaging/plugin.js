c9ade07d386583d8bb1dd1fbb252f34c56f5d56c
Kyle30 <kyle30.lin@gmail.com>
2017-03-14 16:53:01 +0800

    Update plugin.js

--- geoFilteringMessaging/plugin.js
+++ geoFilteringMessaging/plugin.js
@@ -1,6 +1,6 @@
 videojs.plugin('geoFilteringMessaging', function (options) {
     var player = this;
-    player.one('bc-catalog-error', function () {
+    player.on('bc-catalog-error', function () {
         var specificError;
         if (typeof (player.catalog.error) !== 'undefined') {
             specificError = player.catalog.error.data[0];
