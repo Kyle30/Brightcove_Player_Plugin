0c38ee5d57d1a9dca42babd52d69400908475480
Kyle30 <kyle30.lin@gmail.com>
2017-05-23 18:30:20 +0800

    Delete t1.js

--- stillWatching/t1.js
+++
@@ -1,8 +0,0 @@
-videojs.plugin('stillWatching', function () {
-    var player = this;
-    player.on('loadedmetadata', function(){
-      console.log('still watching');
-      var t = player.currentTime();
-      console.log(t);
-    });
-});
