371ffae5f736f40d5d41f49fd64478be134f8993
Kyle30 <kyle30.lin@gmail.com>
2018-01-22 17:35:52 +0800

    Update plugin.js

--- gifPoster/plugin.js
+++ gifPoster/plugin.js
@@ -1,13 +1,11 @@
-videojs.plugin('gifPoster',function(options){
-	var player = this;
-	player.one('loadstart',function(){
-		var vjsPoster = document.getElementsByClassName('vjs-poster')[0];
-// 		remove - Original Poster
-		vjsPoster.style = '';
-// 		append - new Poster
-		var gifImg = document.createElement('img');
-		gifImg.style = options.style;
-		gifImg.src = options.src;
-		vjsPoster.appendChild(gifImg);
-	});
+videojs.plugin('gifPoster', function(options){
+  var player = this;
+  player.one('loadstart', function(){
+    var vjsPoster = document.getElementsByClassName('vjs-poster')[0];
+    vjsPoster.style = '';
+    var gifImg = document.createElement('img');
+    gifImg.style = options.style;
+    gifImg.src = options.src;
+    vjsPoster.appendChild(gifImg);
+  });
 });
